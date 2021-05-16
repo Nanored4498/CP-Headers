c0fbcac2fabcf132c5bab2f512b5c52f72882ba2
Nanored4498 <nanored.mc@gmail.com>
2021-05-16 22:11:40 +0200

    segment tree

---
+++ segment_tree.h
@@ -0,0 +1,55 @@
+#include <vector>
+#include <functional>
+
+using namespace std;
+
+template<typename T>
+struct Fenwick {
+	#define LSB(i) (i)&(-(i))
+	vector<T> v;
+
+	Fenwick(int n): v(n+1, 0) {}
+
+	void add(int i, T x) {
+		for(; i < (int) v.size(); i += LSB(i)) v[i] += x;
+	}
+	void set(int i, T x) {
+		add(i, x - querry(i, i));
+	}
+
+	T querry(int i) {
+		T ans = 0;
+		for(; i > 0; i -= LSB(i)) ans += v[i];
+		return ans;
+	}
+	T querry(int a, int b) {
+		return querry(b) - querry(a-1);
+	}
+};
+
+template<typename T>
+struct SegmentTree {
+	const function<T(T, T)> &op;
+	const T e;
+	vector<T> v;
+	int start;
+
+	SegmentTree(int n, const function<T(T, T)> &op, T e): op(op), e(e), start(1) {
+		while(start < n) start <<= 1;
+		v.assign(start << 1, e);
+	}
+
+	void set(int i, T x) {
+		v[start+i] = x;
+		while((i >>= 1) > 0) v[i] = op(v[i<<1], v[(i<<1)+1]);
+	}
+
+	T querry(int a, int b) {
+		T xa = e, xb = e;
+		for(a += start, b += start; a <= b; a >>= 1, b >>= 1) {
+			if(a&1) xa = op(xa, v[a++]);
+			if(!(b&1)) xb = op(v[b--], xb);
+		}
+		return op(xa, xb);
+	}
+};
